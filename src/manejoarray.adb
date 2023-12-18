540504d764d6596bcd20239b2ceff49e0029f976
Iker <iker4.davila@gmail.com>
2023-12-18 16:17:39 +0100

    comentarios paquetes iker

--- src/manejoarray.adb
+++ src/manejoarray.adb
@@ -164,25 +164,28 @@ procedure rellenarArray(arrayTemp: out tipoArrayTemp; lenArray: in out Integer; 
       
    end mostrarArray; 
    
+   
    procedure Calcular_Temp_media (arrayTemp: in  TipoArrayTemp; lenArray: in Integer) is
-  
+   --2 entradas, arrayTemp = array que se leerá; lenArray = Longitud del array leído.
 
-    --variables internas del proceso
-   -- Variables internas del proceso
+   --variables internas del proceso
    Temp_Media: Float:= 0.0;
    Kont: Integer := 0;
-   --type T_Array is array (1..5) of Float;
+   --Variable utilizada para el cálculo de la parte correspondiente al sumatorio
+   --de una media.
    Tot : Float:= 0.0;
-   
       
    begin
+      --inicialización de variables antes de cada llamamiento
       Temp_Media:= 0.0;
       Tot:= 0.0;
-   for I in 1..lenArray loop
+      --Bucle de 1 al rango del array utilizado
+      for I in 1..lenArray loop
+         --Actualización del valor total que representa el valor del sumatorio en la posición "I".
          Tot := Tot + ArrayTemp(I);   
-   end loop;
-
-   -- Calcular temp. media:
+      end loop;
+      
+   -- Proceso de calcular temperatura media:
    Temp_Media := Tot / Float(lenArray);
 
    -- Mostrar el valor de Temp_Media con dos decimales:
@@ -194,48 +197,52 @@ procedure rellenarArray(arrayTemp: out tipoArrayTemp; lenArray: in out Integer; 
    
    procedure Porcentaje_Dentro_Del_Umbral (arrayTemp: in TipoArrayTemp; lenArray: in integer) is
       
-   -- type T_Umbral is new Float range 0.0..100.0; --límites que el umbral pueda tener, decidido por nosotros
+  --Variables internas del proceso
    
-   Kont, Tot: Integer;
-   Porcentaje: Float;
-   Umbral_Min, Umbral_Max: Float;
+   Kont, Tot: Integer; --Variables usadas como contador, y como total
+   Porcentaje: Float; --Variable que nos indica que porcentaje de temperaturas hay dentro del umbral seleccionado
+   Umbral_Min, Umbral_Max: Float; --Límites máximos y mínimos del umbral
       
    begin
+      --Inicialización del umbral antes de cada llamamiento
       Porcentaje := 0.0;
       Tot := 0;
+      --Petición de límites del umbral
       Put_Line("Escribe un valor para el umbral entre 0 y 100 gracos centrigrados:");Skip_Line(1);
       Put_Line("primero introduce el valor mínimo del umbral y después el máximo:");
       Get(Umbral_Min);
       Get(Umbral_Max);
-   
-      while Umbral_Min < 0.0 or Umbral_Max > 100.0 loop  --se repite si lo mete mal
+      --Mientras el valor añadido en el umbral sea mayor al máximo o menor al mínimo, volverá a pedir valores al usuario.
+      while Umbral_Min < 0.0 or Umbral_Max > 100.0 loop  
          if Umbral_Min < 0.0 then
             Put_Line("el umbral mínimo es menor al permitido, escribe un valor mayor a 0:");
             Get(Umbral_Min);
          elsif Umbral_Max > 100.0 then
             Put_Line("el umbral máximo es mayor al permitido, escribe un valor mayor a 100:"); 
             Get(Umbral_Max);
-         
-         
          else null;
          end if;
       end loop;
+      --Inicialización de variables antes del cálculo del umbral
       Kont:= 0;
       Tot:= 0;
-      --sale del while y ejecuta lo siguiente:
+      --Mensaje que indica que el valor del umbral introducido está dentro del permitido
       Put_Line("valor del umbral admitido, procediendo al cálculo:");
       Skip_Line(1);
       Put_Line("==================================================");
       --cálculo del porcentaje dentro del umbral
       for I in 1..lenArray loop
          Tot:= Tot + 1;
+         --Si el valor del array en la posición "I" se encuentra comprendido entre el valor mínimo
+         -- y máximo del umbral, se sumará 1 a la variable de contador.
          if arrayTemp(I) >= Umbral_Min and arrayTemp(I) <= Umbral_Max then
             Kont:= Kont + 1;
          end if;
       end loop;
-      --porcentaje:
-   Porcentaje := ((Float(Kont) / Float( Tot)) * 100.0);
-   Put_Line("el porcentaje de valores dentro del umbral escogido es del:");
+      --porcentaje: Se divide el número que el contador contiene y se divide entre todos los valores
+      --Leídos en el array.
+    Porcentaje := ((Float(Kont) / Float( Tot)) * 100.0);
+    Put_Line("el porcentaje de valores dentro del umbral escogido es del:");
 
       Put(Item => Porcentaje, Fore => 3, Aft => 2, Exp => 0); Put_Line(" %."); 
       
